25abed0d0f1f1e9ae3cd3e0591bb9ca8b3eca62d
Franco Tegaldi <francoteg+1@gmail.com>
2026-08-30 13:48:32 +0900

    add enemy spawner i guess

--- Source/YowYow/Public/BattleSystem/WaveEnemyManager.h
+++ Source/YowYow/Public/BattleSystem/WaveEnemyManager.h
@@ -1,17 +1,47 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Containers/Set.h"
 #include "GameFramework/Actor.h"
 #include "WaveEnemyManager.generated.h"
+
+class AActor;
+class AEnemyCharacter;
+class AEnemySpawnPoint;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnEncounterCompleted);
+
+USTRUCT(BlueprintType)
+struct FEnemyWaveEntry
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Encounter")
+	TSubclassOf<AEnemyCharacter> EnemyClass;
+
+	/** 0..n into SpawnPoints, or -1 to pick automatically (unused path, not closest to player). */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Encounter", meta = (ClampMin = "-1"))
+	int32 SpawnPointIndex = -1;
+};
+
+USTRUCT(BlueprintType)
+struct FEnemyWave
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Encounter")
+	TArray<FEnemyWaveEntry> Entries;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Encounter", meta = (ClampMin = "0.0"))
+	float DelayBeforeWave = 1.5f;
+};
 
 UCLASS()
 class YOWYOW_API AWaveEnemyManager : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	AWaveEnemyManager();
 
 	UFUNCTION(BlueprintCallable, Category = "Enemy Wave")
@@ -23,19 +53,65 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Enemy Wave")
 	void RegisterEnemyDefeated(AActor* DefeatedEnemy);
 
+	UFUNCTION(BlueprintCallable, Category = "Encounter")
+	void StartEncounter();
+
+	UFUNCTION(BlueprintPure, Category = "Encounter")
+	bool IsEncounterCompleted() const { return bEncounterCompleted; }
+
+	UFUNCTION(BlueprintPure, Category = "Encounter")
+	bool IsEncounterStarted() const { return bEncounterStarted; }
+
+	UPROPERTY(BlueprintAssignable, Category = "Encounter")
+	FOnEncounterCompleted OnEncounterCompleted;
+
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Enemy Wave")
-	int32 TotalEnemiesToDefeat = 30;
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Encounter")
+	bool bAutoStart = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Encounter", meta = (ClampMin = "0.0", EditCondition = "bAutoStart"))
+	float EncounterStartDelay = 1.f;
+
+	/** Used when a wave entry has no class set. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Encounter")
+	TSubclassOf<AEnemyCharacter> DefaultEnemyClass;
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Encounter")
+	TArray<TObjectPtr<AEnemySpawnPoint>> SpawnPoints;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Encounter")
+	TArray<FEnemyWave> Waves;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Enemy Wave")
-	int32 MaxAttackTokens = 3;
+	int32 MaxAttackTokens = 2;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Enemy Wave")
 	int32 DefeatedEnemies = 0;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Encounter")
+	int32 CurrentWaveIndex = -1;
+
 	UPROPERTY()
-	TArray<AActor*> EnemiesWithAttackTokens;
-private:	
-	// Called every frame
-	void CheckWaveComplete();
+	TArray<TObjectPtr<AActor>> EnemiesWithAttackTokens;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> AliveEnemies;
+
+private:
+	bool bEncounterStarted = false;
+	bool bEncounterCompleted = false;
+
+	FTimerHandle EncounterStartTimerHandle;
+	FTimerHandle SpawnWaveTimerHandle;
+
+	void AdvanceWave();
+	void SpawnCurrentWave();
+	void CompleteEncounter();
+
+	TSubclassOf<AEnemyCharacter> ResolveEnemyClass(const FEnemyWaveEntry& Entry) const;
+	AEnemySpawnPoint* PickSpawnPoint(int32 RequestedIndex, TSet<int32>& UsedThisWave) const;
+	void SpawnEnemy(TSubclassOf<AEnemyCharacter> EnemyClass, AEnemySpawnPoint* SpawnPoint);
 };
